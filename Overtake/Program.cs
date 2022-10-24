417d9b55dee5e68880588db8eb4b9b7535af1cb0
Jamie Myland <jamie.myland@gmail.com>
2022-10-24 17:55:05 +0100

    Enabled random dataset generation as default

--- Overtake/Program.cs
+++ Overtake/Program.cs
@@ -9,9 +9,11 @@ namespace Overtake
             // This example fetches and prints the first 10 data items
             for (var i = 0; i < 5; i++)
             {
-                //Library.Overtake.SetRandomAsRepeatable(false);
+                Blackbox.Overtake.SetRandomAsRepeatable(false); //Ensures a different set of data is generated each time. Set to true to make the generated data static
                 var overtake = Blackbox.Overtake.GetNextOvertake();
-                //Console.WriteLine($"{overtake.ToString()}\n");
+                //Console.WriteLine($"{overtake.ToString()}\n"); //Output all data without formatting
+
+                //Output data WITH formatting:
                 Console.WriteLine($"InitialSeparation = {overtake.InitialSeparationM:F1} metres");
                 Console.WriteLine($"OvertakingSpeed = {overtake.OvertakingSpeedMPS:F1} m/s");
                 Console.WriteLine($"OncomingSpeed = {overtake.OncomingSpeedMPS:F1} m/s");
