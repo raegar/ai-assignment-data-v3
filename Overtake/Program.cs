ca11f48e9b2f59c934b9f8393f96cd219e707869
Jamie Myland <jamie.myland@gmail.com>
2022-10-24 17:44:03 +0100

    Added overtake with variance code

---
+++ Overtake/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Overtake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This example fetches and prints the first 10 data items
+            for (var i = 0; i < 5; i++)
+            {
+                //Library.Overtake.SetRandomAsRepeatable(false);
+                var overtake = Blackbox.Overtake.GetNextOvertake();
+                //Console.WriteLine($"{overtake.ToString()}\n");
+                Console.WriteLine($"InitialSeparation = {overtake.InitialSeparationM:F1} metres");
+                Console.WriteLine($"OvertakingSpeed = {overtake.OvertakingSpeedMPS:F1} m/s");
+                Console.WriteLine($"OncomingSpeed = {overtake.OncomingSpeedMPS:F1} m/s");
+                Console.WriteLine($"Success = {overtake.Success}\n");
+            }
+            
+            Console.ReadKey(); // Keep the window open till a key is pressed
+        }
+    }
+}
